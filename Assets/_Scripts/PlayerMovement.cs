4bdaef80f79befec9dc19c8b65bd90a118d72f94
fa019 <fa019@hdm-stuttgart.de>
2019-05-13 11:30:54 +0200

    Implement Look At mouse

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveVelocity;
     private Rigidbody2D rb;
     public Animator anim;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +21,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousepos = Input.mousePosition;
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
         anim.SetFloat("VelocityX", movement.x);
         anim.SetFloat("VelocityY", movement.y);
         anim.SetFloat("Magnitude", movement.magnitude);
 
 
-        if(mousepos.x < this.transform.position.x)
+        
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        
+
+        if (mousePos.x < this.transform.position.x)
         {
             this.transform.localScale = new Vector3(-1f, 1f, 1f);
         } else
