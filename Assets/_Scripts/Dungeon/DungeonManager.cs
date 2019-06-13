02910228badc08eec45d0fe1efc34c6f71d94b13
fa019 <fa019@hdm-stuttgart.de>
2019-06-13 16:33:32 +0200

    created PGD

--- Assets/_Scripts/Dungeon/DungeonManager.cs
+++ Assets/_Scripts/Dungeon/DungeonManager.cs
@@ -1,10 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DungeonManager : MonoBehaviour
 {
-    public GameObject floorPrefab, wallPrefab;
+    public GameObject floorPrefab, wallPrefab, tilePrefab;
+    public int totalFloorCount;
 
     [HideInInspector] public float minX, maxX, minY, maxY;
+
+    List<Vector3> floorList = new List<Vector3>();
+
+    private void Start()
+    {
+        RandomWalker();
+    }
+
+    private void Update()
+    {
+        if (Application.isEditor && Input.GetKeyDown(KeyCode.Backspace))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void RandomWalker()
+    {
+        Vector3 curPos = Vector3.zero;
+        floorList.Add(curPos);
+        while (floorList.Count < totalFloorCount)
+        {
+            switch (Random.Range(1, 5))
+            {
+                case 1: curPos += Vector3.up; break;
+                case 2: curPos += Vector3.right; break;
+                case 3: curPos += Vector3.down; break;
+                case 4: curPos += Vector3.left; break;
+            }
+            bool inFloorList = false;
+            for(int i = 0; i < floorList.Count; i++)
+            {
+                if(Vector3.Equals(curPos, floorList[i]))
+                {
+                    inFloorList = true;
+                    break;
+                }
+            }
+            if (!inFloorList)
+            {
+                floorList.Add(curPos);
+            }
+        }
+        for(int i = 0; i < floorList.Count; i++)
+        {
+            GameObject goTile = Instantiate(tilePrefab, floorList[i], Quaternion.identity) as GameObject;
+            goTile.name = tilePrefab.name;
+            goTile.transform.SetParent(transform);
+        }
+    }
 }
--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -9,25 +9,31 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveVelocity;
     private Rigidbody2D rb;
     public Animator anim;
-    
+    LayerMask obstacleMask;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
+        obstacleMask = LayerMask.GetMask("Wall", "Enemy");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        move();
+    }
+
+    void move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f) * Speed;
         anim.SetFloat("VelocityX", movement.x);
         anim.SetFloat("VelocityY", movement.y);
         anim.SetFloat("Magnitude", movement.magnitude);
 
 
-        
+
         Vector3 mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
