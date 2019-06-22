e0ea53dc99dc1cdfcf2cdfd68ebfcae97c311743
fa019 <fa019@hdm-stuttgart.de>
2019-06-22 18:38:18 +0200

    raycast

--- Assets/_Scripts/Enemy/ShootingEnemy.cs
+++ Assets/_Scripts/Enemy/ShootingEnemy.cs
@@ -14,37 +14,59 @@ public class ShootingEnemy : MonoBehaviour
 
     public GameObject projectile; 
 
-    private Transform player; 
+    private Transform player;
+    private int floorMask;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        timeBtwShots = startTimeBtwShots; 
+        timeBtwShots = startTimeBtwShots;
+        Physics2D.queriesStartInColliders = false;
+        floorMask = LayerMask.GetMask("Floor");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        
-            if(Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        Vector3 dir = -(this.transform.position - player.transform.position).normalized;
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, dir, startTargeting, floorMask);
+        if(hitInfo.collider != null && hitInfo.collider.CompareTag("Player"))
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.green);
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                print("hit player");
+            }
+        } else
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+        }
+
+
+
+        if (Vector2.Distance(transform.position, player.position) < startTargeting) {
+
+            if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
             {
                 transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-            }  else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance) {
+            } else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance) {
                 transform.position = this.transform.position;
             } else if (Vector2.Distance(transform.position, player.position) < retreatDistance) {
                 transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
-        }
-        
-       
-        if (timeBtwShots <= 0 && (Vector2.Distance(transform.position, player.position) < startTargeting))
-        {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            timeBtwShots = startTimeBtwShots;
-        } else
-        {
-            timeBtwShots -= Time.deltaTime; 
+            }
+
+
+            if (timeBtwShots <= 0)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBtwShots = startTimeBtwShots;
+            } else
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
         }
     }
 }
